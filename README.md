c632d931a51387914ecc442ea9c65aefcdcd0d08
Raf Alvarado <ontoligent@gmail.com>
2015-03-28 10:27:56 -0400

    Reinit commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# Synopsis
+
+Takes a csv file that maps semantic entities to document elements and 
+generates an entity graph following the common container correlation 
+pattern (Alvarado 2015). This graph may then be analyzed for its network 
+properties of visualized in a variery of ways.
+
+# Desing Notes
+
+* Didactic code, i.e. verbose. Could have been written as one-liners.
+
+# Object Model
+
+Objects:
+  EntityElementIndex
+    Description 
+      A CSV file that contains an Entity to Element index
+      Must contain at least two columns -- Entity ID and Element ID
+      May also containt Entity Class and Element Class cols as well
+      If the class columns are not present, a default type for each should be given
+    Methods
+      load_file
+      set_element_id_col
+      set_element_class_col
+      set_entity_id_col
+      set_entity_class_col
+      set_default_element_class
+      set_default_entity_class
+      get_row_count
+      get_col_count
+      get_element_classes
+      get_distinct_entity_ids
+      get_distinct_element_ids
+  EntityGraphConfig
+    Description
+      Defines which cols and filters will be used to construct the graph
+    Methods
+      set_middle_term_col
+      set_subject_term_col
+      set_object_term_col
+      set_subject_term_filter_col
+      set_object_term_filter_col      
+      set_subject_term_filter_vals
+      set_object_term_filter_vals
+      get_config
+  EntityGraph
+    Description
+    Methods
+      set_index (object)
+      set_config (object)
+      generate_graph  
+      get_properties
+  EntityGraphViz
+    Methods
+      set_graph (object)
+      
+      
---
+++ egg.py
@@ -0,0 +1,116 @@
+'''
+EGG, an Entity Graph Generator
+'''
+
+import pandas as pd
+
+class EntityElementIndex():
+
+	filename = ''
+	dataframe = ''
+	element_id_col = 'element_id'
+	element_class_col = 'element_class'
+	entity_id_col = 'entity_id'
+	entity_class_col = 'entity_class'
+	default_element_class = 'element'
+	default_entity_class= 'entity'
+	
+	def set_csvfile(self,filename):
+		self.filename = filename
+		self.dataframe = pd.read_csv(self.filename)
+		
+	def set_element_id_col(self,colname):
+		self.element_id_col = colname
+
+	def set_element_class_col(self,colname):
+		self.element_class_col = colname
+
+	def set_entity_id_col(self,colname):
+		self.entity_id_col = colname
+
+	def set_entity_class_col(self,colname):
+		self.entity_class_col = colname
+
+	def set_default_element_class(self,classname):
+		self.default_element_class = classname
+
+	def set_default_entity_class(self,classname):
+		self.default_entity_class = classname
+
+	def get_row_count(self):
+		return len(self.dataframe[:])
+
+	def get_col_count(self):
+		cols = self.dataframe.columns.values.tolist()
+		return len(cols)
+		
+	def get_dataframe(self):
+	  return self.dataframe
+
+	def get_element_classes(self):
+		return 1
+
+	def get_distinct_entity_ids(self):
+		return 1
+
+	def get_distinct_element_ids(self):
+		return 1
+		
+	def get_dataframe(self):
+		return self.dataframe
+
+
+class EntityGraphConfig(object):
+
+	def set_middle_term_col(self,colname):
+		return 1
+
+	def set_subject_term_col(self,colname):
+		return 1
+
+	def set_object_term_col(self,colname):
+		return 1
+
+	def set_subject_term_filter_col(self,colname):
+		return 1
+
+	def set_object_term_filter_col(self,colnanme):
+		return 1
+
+	def set_subject_term_filter_vals(self,vals):
+		return 1
+
+	def set_object_term_filter_vals(self,vals):
+		return 1
+
+	def get_config(self):
+		return 1
+
+
+class EntityGraph(object):
+
+	def set_index(self, index_obj):
+		return 1
+
+	def set_config(self, config_obj):
+		return 1
+
+	def generate_graph(self):
+		return 1
+
+	def get_properties(self):
+		return 1
+
+
+class EntityGraphViz(object):
+
+	def set_graph(self, graph_obj):
+		return 1
+
+	
+if __name__ == '__main__':
+	my_root = '/Users/rca2t/Dropbox/PLAY/egg/demo/'
+	my_demo ='charrette.csv' 
+	idx = EntityElementIndex()
+	idx.set_csvfile(my_root + my_demo)
+	print idx.get_col_count(), idx.get_row_count()
